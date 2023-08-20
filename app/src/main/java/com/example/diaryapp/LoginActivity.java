f07230eba98df3c411e299549be966fe0a7e5ba8
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2023-08-20 19:38:19 +0800

    Update login activity
    1. Add intent from LoginActivity to MainActivity.
    2. Make LoginActivity as the main entry point of the app (main launcher activity).

--- app/src/main/java/com/example/diaryapp/LoginActivity.java
+++ app/src/main/java/com/example/diaryapp/LoginActivity.java
@@ -2,13 +2,32 @@ package com.example.diaryapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private MaterialButton mSignInButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mSignInButton = findViewById(R.id.google_login_button);
+
+        View.OnClickListener signInClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+                Toast.makeText(LoginActivity.this, "Hello new User!", Toast.LENGTH_SHORT).show();
+            }
+        };
+
+        mSignInButton.setOnClickListener(signInClickListener);
     }
 }
