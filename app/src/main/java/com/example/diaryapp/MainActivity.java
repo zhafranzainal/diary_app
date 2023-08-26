7057ac01a07dacf552ded6c671e8a8a129dbd853
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2023-08-26 20:33:27 +0800

    Add logout in main activity

--- app/src/main/java/com/example/diaryapp/MainActivity.java
+++ app/src/main/java/com/example/diaryapp/MainActivity.java
@@ -1,14 +1,49 @@
 package com.example.diaryapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
+    private MaterialToolbar mToolbar;
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+        mToolbar = findViewById(R.id.main_activity_toolbar);
+
+        mToolbar.getMenu().getItem(0).setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+
+            @Override
+            public boolean onMenuItemClick(@NonNull MenuItem item) {
+
+                FirebaseUser user = mAuth.getCurrentUser();
+
+                if (user != null) {
+                    mAuth.signOut();
+                    Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(intent);
+                }
+
+                return true;
+            }
+
+        });
+
     }
+
 }
