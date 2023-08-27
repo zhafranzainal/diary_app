4716954e33efb5fbc9d848ac89bb9809344ade64
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2023-08-27 19:34:05 +0800

    Implement back navigation for AddDiaryActivity

--- app/src/main/java/com/example/diaryapp/AddDiaryActivity.java
+++ app/src/main/java/com/example/diaryapp/AddDiaryActivity.java
@@ -2,13 +2,33 @@ package com.example.diaryapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.appbar.MaterialToolbar;
 
 public class AddDiaryActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_diary);
+
+        MaterialToolbar toolbar = findViewById(R.id.add_diary_toolbar);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+
+        });
+
     }
+
 }
