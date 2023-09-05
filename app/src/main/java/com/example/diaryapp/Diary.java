6827a638787aa026b02462b99bac7ff10dcef8d0
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2023-09-06 07:32:58 +0800

    Create Diary.java

---
+++ app/src/main/java/com/example/diaryapp/Diary.java
@@ -0,0 +1,58 @@
+package com.example.diaryapp;
+
+public class Diary {
+
+    private String diaryId, title, note, type, image, placeName;
+
+    public Diary() {
+    }
+
+    public String getDiaryId() {
+        return diaryId;
+    }
+
+    public void setDiaryId(String diaryId) {
+        this.diaryId = diaryId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getPlaceName() {
+        return placeName;
+    }
+
+    public void setPlaceName(String placeName) {
+        this.placeName = placeName;
+    }
+
+}
