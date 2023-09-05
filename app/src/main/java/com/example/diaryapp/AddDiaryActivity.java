a39810cb8c928938712d2fe5bfa1cebdab064bfc
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2023-09-05 16:44:06 +0800

    Complete add diary activity

--- app/src/main/java/com/example/diaryapp/AddDiaryActivity.java
+++ app/src/main/java/com/example/diaryapp/AddDiaryActivity.java
@@ -16,14 +16,32 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.bitmap.CenterCrop;
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.progressindicator.CircularProgressIndicator;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.util.HashMap;
 
 public class AddDiaryActivity extends AppCompatActivity {
+
+    private FirebaseUser mUser;
+    private DatabaseReference mDatabaseRef;
 
     private final String mStoragePermission = Manifest.permission.READ_EXTERNAL_STORAGE;
     private final int mPermissionGranted = PackageManager.PERMISSION_GRANTED;
@@ -32,7 +50,10 @@ public class AddDiaryActivity extends AppCompatActivity {
     private static final int IMAGE_PICK_GALLERY_CODE = 20;
 
     private ImageView mDiaryImage;
-    private TextInputEditText mDiaryLocation;
+    private TextInputEditText mDiaryTitle, mDiaryNote, mDiaryLocation;
+    private MaterialButton mSaveDiaryButton;
+    private CircularProgressIndicator mProgressIndicator;
+    private Uri mSelectedImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,9 +61,18 @@ public class AddDiaryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_diary);
 
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance("https://diary-app-fdcfc-default-rtdb.asia-southeast1.firebasedatabase.app");
+        mDatabaseRef = firebaseDatabase.getReference();
+        mUser = firebaseAuth.getCurrentUser();
+
         MaterialToolbar toolbar = findViewById(R.id.add_diary_toolbar);
+        mDiaryTitle = findViewById(R.id.add_diary_title);
+        mDiaryNote = findViewById(R.id.add_diary_note);
         mDiaryImage = findViewById(R.id.add_diary_image);
         mDiaryLocation = findViewById(R.id.add_diary_location);
+        mSaveDiaryButton = findViewById(R.id.save_diary_button);
+        mProgressIndicator = findViewById(R.id.add_diary_progress_bar);
 
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
 
@@ -61,6 +91,43 @@ public class AddDiaryActivity extends AppCompatActivity {
             public boolean onMenuItemClick(@NonNull MenuItem item) {
                 displayOptionBuilder();
                 return false;
+            }
+
+        });
+
+        mSaveDiaryButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                String title = mDiaryTitle.getText().toString();
+                String note = mDiaryNote.getText().toString();
+                String location = mDiaryLocation.getText().toString();
+
+                if (!title.isEmpty() && !note.isEmpty()) {
+
+                    if (isTextDiary(location)) {
+                        if (mUser != null) {
+                            mSaveDiaryButton.setClickable(false);
+                            mProgressIndicator.setVisibility(View.VISIBLE);
+                            mProgressIndicator.setProgressCompat(500, true);
+                            saveTextDiaryToDatabase(title, note, mUser.getUid());
+                        }
+                    } else if (isImageDiary(location)) {
+                        if (mUser != null) {
+                            mSaveDiaryButton.setClickable(false);
+                            mProgressIndicator.setVisibility(View.VISIBLE);
+                            mProgressIndicator.setProgressCompat(300, true);
+                            saveImageDiaryToStorage(title, note, mSelectedImage, location, mUser.getUid());
+                        }
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Please fill out all fields!", Toast.LENGTH_SHORT).show();
+                    }
+
+                } else {
+                    Toast.makeText(getApplicationContext(), "Please enter both title and note.", Toast.LENGTH_SHORT).show();
+                }
+
             }
 
         });
@@ -134,10 +201,10 @@ public class AddDiaryActivity extends AppCompatActivity {
 
             if (data != null) {
 
-                Uri selectedImage = data.getData();
+                mSelectedImage = data.getData();
 
                 Glide.with(this)
-                        .load(selectedImage)
+                        .load(mSelectedImage)
                         .transition(DrawableTransitionOptions.withCrossFade())
                         .transform(new CenterCrop())
                         .into(mDiaryImage);
@@ -149,6 +216,152 @@ public class AddDiaryActivity extends AppCompatActivity {
             }
 
         }
+
+    }
+
+    private boolean isTextDiary(String location) {
+        return mSelectedImage == null && location.isEmpty();
+    }
+
+    private boolean isImageDiary(String location) {
+        return mSelectedImage != null && !location.isEmpty();
+    }
+
+    private void saveTextDiaryToDatabase(String title, String note, String userId) {
+
+        HashMap<String, Object> diaryHashmap = new HashMap<>();
+        diaryHashmap.put("title", title);
+        diaryHashmap.put("note", note);
+        diaryHashmap.put("type", "text");
+
+        DatabaseReference diaryNode = mDatabaseRef.child(userId).push();
+        String diaryNodeId = diaryNode.getKey();
+        diaryHashmap.put("diaryId", diaryNodeId);
+
+        mProgressIndicator.setVisibility(View.VISIBLE);
+        mProgressIndicator.setProgressCompat(100, true);
+
+        diaryNode.updateChildren(diaryHashmap).addOnCompleteListener(new OnCompleteListener<Void>() {
+
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+
+                if (task.isSuccessful()) {
+
+                    mProgressIndicator.setVisibility(View.INVISIBLE);
+                    Toast.makeText(getApplicationContext(), "Diary added successfully!", Toast.LENGTH_SHORT).show();
+
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(intent);
+
+                }
+
+            }
+
+        }).addOnFailureListener(new OnFailureListener() {
+
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                mSaveDiaryButton.setClickable(true);
+            }
+
+        });
+
+    }
+
+    private void saveImageDiaryToStorage(String title, String note, Uri selectedImage, String location, String userId) {
+
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        StorageReference storageRef = storage.getReference();
+
+        storageRef.child(userId).child(selectedImage.getLastPathSegment()).putFile(selectedImage).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+
+            @Override
+            public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+
+                if (task.isSuccessful()) {
+
+                    task.getResult().getMetadata().getReference().getDownloadUrl().addOnCompleteListener(new OnCompleteListener<Uri>() {
+
+                        @Override
+                        public void onComplete(@NonNull Task<Uri> task) {
+                            if (task.isSuccessful()) {
+                                String imageURL = task.getResult().toString();
+                                saveImageDiaryToDatabase(title, note, imageURL, location, userId);
+                            }
+                        }
+
+                    }).addOnFailureListener(new OnFailureListener() {
+
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                            mSaveDiaryButton.setClickable(true);
+                        }
+
+                    });
+
+                }
+
+            }
+
+        }).addOnFailureListener(new OnFailureListener() {
+
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                mSaveDiaryButton.setClickable(true);
+            }
+
+        });
+
+    }
+
+    private void saveImageDiaryToDatabase(String title, String note, String imageURL, String location, String userId) {
+
+        HashMap<String, Object> diaryHashmap = new HashMap<>();
+        diaryHashmap.put("title", title);
+        diaryHashmap.put("note", note);
+        diaryHashmap.put("type", "image");
+        diaryHashmap.put("image", imageURL);
+        diaryHashmap.put("placeName", location);
+
+        DatabaseReference diaryNode = mDatabaseRef.child(userId).push();
+        String diaryNodeId = diaryNode.getKey();
+        diaryHashmap.put("diaryId", diaryNodeId);
+
+        mProgressIndicator.setVisibility(View.VISIBLE);
+        mProgressIndicator.setProgressCompat(100, true);
+
+        diaryNode.updateChildren(diaryHashmap).addOnCompleteListener(new OnCompleteListener<Void>() {
+
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+
+                if (task.isSuccessful()) {
+
+                    mProgressIndicator.setVisibility(View.INVISIBLE);
+                    Toast.makeText(getApplicationContext(), "Diary added successfully!", Toast.LENGTH_SHORT).show();
+
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(intent);
+
+                }
+
+            }
+
+        }).addOnFailureListener(new OnFailureListener() {
+
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                mSaveDiaryButton.setClickable(true);
+            }
+
+        });
 
     }
 
